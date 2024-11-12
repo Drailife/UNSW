ec70cdfcf61a9dce3cde7d7f4fce8554572bd9b9
dbb-speech-ubuntu <drailife@gmail.com>
2024-11-12 12:09:54 +0800

    update stage 4.1

--- cs_dungeon.c
+++ cs_dungeon.c
@@ -815,7 +815,7 @@ void print_basic_dungeon(struct dungeon *dungeon, char *player_name,
     printf("Monster: %s\n", monster_string(dungeon->monster));
 
     if (dungeon->contains_player) {
-        printf("--%s is here\n", player_name);
+        printf("%s is here\n", player_name);
     } else {
         printf("Empty\n");
     }
